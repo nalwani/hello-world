3c88d6aa747d5d0ad8579948371def0c2a601a06
nalwani <N.M.Alwani@student.tudelft.nl>
2022-09-21 16:02:49 +0200

    add comments that came from thought process

--- src/hello.py
+++ src/hello.py
@@ -2,6 +2,16 @@ def main(y):
     print(f"Hello, {y}!")
 
 
+# read text from storage
+# split text into words
+# save words
+
+# read stored text
+# recognise important words
+# for each important word generate a list of words
+# save pair with  the important words as keys and the list of generated words as value
+
+
 x = "Neil"
 
 if __name__ == '__main__':
